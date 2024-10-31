89c93853b2cd47b328ede11f7705e8df0da679ed
SeungtaekLim <0530lim0530@gmail.com>
2024-10-31 19:42:20 +0900

    스프링부트 추가

---
+++ watchu/watchu/src/test/java/com/wc/watchu/WatchuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wc.watchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
