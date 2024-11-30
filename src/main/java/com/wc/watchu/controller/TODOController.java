b7c468a3b598e9fa725ce0401b2dffc3bb53a768
SeungtaekLim <0530lim0530@gmail.com>
2024-11-30 19:28:40 +0900

    나머지 등록

---
+++ src/main/java/com/wc/watchu/controller/TODOController.java
@@ -0,0 +1,38 @@
+package com.wc.watchu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TODOController {
+
+    @GetMapping("/admin_registration")
+    public String adminRegistration() {
+        return "admin/admin_registration";
+    }
+
+    @GetMapping("/admin_read")
+    public String adminRead() {
+        return "admin/admin_read";
+    }
+
+    @GetMapping("/director_add")
+    public String directorAdd() {
+        return "admin/director_add";
+    }
+
+    @GetMapping("/director_read")
+    public String directorRead() {
+        return "admin/director_add";
+    }
+
+    @GetMapping("/actor_add")
+    public String actorAdd() {
+        return "admin/actor_add";
+    }
+
+    @GetMapping("/actor_read")
+    public String actorRead() {
+        return "admin/actor_add";
+    }
+}
